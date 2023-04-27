94eaa5841d11341f86f266b06715af5b106387d4
citypilgrim <ciudadperegrino@gmail.com>
2023-04-27 12:02:07 +0800

    update emacs packages

--- examples/src/citypilgrim/packages.scm
+++ examples/src/citypilgrim/packages.scm
@@ -51,6 +51,8 @@
          "font-gnu-freefont"
 
          ;; emacs dependencies
+         "emacs-transient"              ;shared
+         ;; "emacs-dired-hacks"              ;incompatible
          "emacs-use-package"            ;package
          "emacs-gcmh"                   ;performance
          "emacs-no-littering"           ;defaults
@@ -64,19 +66,21 @@
          "emacs-vertico"
          "emacs-orderless"
          "emacs-marginalia"
-         "emacs-tempel"
+         "emacs-tempel" "emacs-tempel-collection"
          "emacs-yasnippet"
          "emacs-yasnippet-snippets"
          "emacs-consult"
          "emacs-embark"
          "emacs-undo-fu"                ;ux
-         ;; "emacs-tabspaces"
+         ;; "emacs-tabspaces"              ;incompatible
          "emacs-popper"
          "emacs-helpful" "emacs-elisp-demos"
          "emacs-emojify"                ;ui
-         "emacs-all-the-icons" "emacs-all-the-icons-dired"
+         "emacs-all-the-icons" "emacs-all-the-icons-dired" "emacs-all-the-icons-completion"
+         "emacs-doom-modeline"
          "emacs-doom-themes" "emacs-ef-themes"
          "emacs-flycheck"               ;ide
+         ;; "emacs-groovy-modes"           ;incompatible
          "emacs-pyvenv"
          "emacs-dockerfile-mode"
          "emacs-eglot"
@@ -90,11 +94,13 @@
          "emacs-magit"                  ;vc
          "emacs-forge"
          "emacs-magit-todos"
-         "emacs-git-gutter"
+         ;; "emacs-magit-popup"            ;incompatible
+         "emacs-git-gutter" "emacs-git-gutter-fringe" "emacs-fringe-helper"
          "emacs-guix"
-         "emacs-citar"                  ;books
-         ;; "emacs-citar-embark"
-         ;; "emacs-nov"
+         "emacs-citar" ;books
+         ;; "emacs-citar-embark"           ;missing
+         ;; "emacs-nov-el"                 ;incompatible
+         "emacs-esxml"                  ;dep for nov
          ;; "emacs-dtk"
          "emacs-json-mode"              ;data
          "emacs-csv-mode"
@@ -108,10 +114,10 @@
          "emacs-org-tree-slide"
          "emacs-org-roam"               ;notes
          "emacs-emacsql-sqlite3"
-         ;; "emacs-org-roam-ui"
+         ;; "emacs-org-roam-ui"            ;missing
          "emacs-restclient"             ;web
          "emacs-cdlatex"                ;latex
-
          "emacs-telega"                 ;msg
          "gperf" "cmake" "pkg-config"
+         "emacs-gptel"                  ;ai
          )))
